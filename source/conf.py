5261d8d77a4a8e5b8be69d1044f818d78bbb8177
Aboboo <docs@aboboo.com>
2012-08-24 14:58:06 +0800

    修改language和html_search_language

--- source/conf.py
+++ source/conf.py
@@ -244,3 +244,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+language = 'zh_CN'
+html_search_language = 'ja'
